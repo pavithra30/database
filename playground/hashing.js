c1a0be9fd5333b98601a7dc06f8a831ff0b1a593
pavithra30 <pavithra.t@evolvussolutions.com>
2018-05-09 12:52:37 +0530

    save method using hashing password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,9 +1,22 @@
 const {SHA256}=require('crypto-js');
-const jwt=require('jsonwebtoken');  
+const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
+
+
+var password='123abc!';
+/*bcrypt.genSalt(10,(err,salt)=>{
+  bcrypt.hash(password,salt,(err,hash)=>{
+    console.log(hash);
+  });
+});*/
+var hashedPassword='$2a$10$cl68P7y3ee5Ica3AFlf9/ujfv09NZWGtHiLhc360UiUeAPJ8V.XdC';
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+  console.log(res);
+})
 
 
 
-var data={
+/*var data={
   id:10
 };
 var token=jwt.sign(data, '123abc');
@@ -13,7 +26,7 @@ var decoded=jwt.verify(token, '123abc');
 console.log('decoded',decoded);
 
 
-/*var message='I am user number 3';
+var message='I am user number 3';
 var hash=SHA256(message).toString();
 
 console.log(`message : ${message}`);
